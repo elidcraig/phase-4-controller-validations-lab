c6d1a14ac57f8623fcbae02ee85384210df864d6
Eli Craig <elidcraig@gmail.com>
2022-06-29 19:06:51 -0700

    commit tree 401e4e32f21eea935bb7c06462e5fb80d5f2471a

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::API
+  rescue_from ActiveRecord::RecordInvalid, with: :handle_invalid
+
+  private
+
+  def handle_invalid e
+    render json: { errors: e.record.messages }, status: :unprocessable_entity
+  end
 end
